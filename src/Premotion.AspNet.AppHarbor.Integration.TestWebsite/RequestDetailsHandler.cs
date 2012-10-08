23ea8a069459e852b2300b13dab669f16db6522b
Bert Willems <b.willems@premotion.nl>
2012-10-08 09:19:33 +0200

    + added more debug info

--- src/Premotion.AspNet.AppHarbor.Integration.TestWebsite/RequestDetailsHandler.cs
+++ src/Premotion.AspNet.AppHarbor.Integration.TestWebsite/RequestDetailsHandler.cs
@@ -37,13 +37,44 @@ namespace Premotion.AspNet.AppHarbor.Integration.TestWebsite
 			output.WriteLine("### Server variables");
 			var serverVariables = request.ServerVariables;
 			foreach (var key in serverVariables.AllKeys)
-				output.WriteLine(string.Format("{0}: {1}", key, serverVariables[key]));
+			   output.WriteLine(string.Format("{0}: {1}", key, serverVariables[key]));
 
 			// dump request headers
 			output.WriteLine("### Request headers");
 			var headerVariables = request.Headers;
 			foreach (var key in headerVariables.AllKeys)
-				output.WriteLine(string.Format("{0}: {1}", key, headerVariables[key]));
+			   output.WriteLine(string.Format("{0}: {1}", key, headerVariables[key]));
+
+			// dump query string
+			output.WriteLine("### Querystring (GET)");
+			var queryStringVariables = request.QueryString;
+			foreach (var key in queryStringVariables.AllKeys)
+			   output.WriteLine(string.Format("{0}: {1}", key, queryStringVariables[key]));
+
+			// dump POST
+			output.WriteLine("### Form variables (POST)");
+			var formVariables = request.Form;
+			foreach (var key in formVariables.AllKeys)
+			   output.WriteLine(string.Format("{0}: {1}", key, formVariables[key]));
+
+			// dump Session
+			output.WriteLine("### Session");
+			var session = context.Session;
+			if (session == null)
+			   output.WriteLine("No session");
+			else
+			{
+			   var count = session.Count;
+			   output.WriteLine("Count: {0}\r\n", count);
+			   foreach (string key in session.Keys)
+			      output.WriteLine(string.Format("{0}: {1}", key, session[key]));
+			}
+
+			// dump cookies
+			output.WriteLine("### Cookies");
+			var cookieCollection = request.Cookies;
+			foreach (var key in cookieCollection.AllKeys)
+			   output.WriteLine(string.Format("{0}: {1}", key, cookieCollection[key]));
 		}
 		/// <summary>
 		/// Gets a value indicating whether another request can use the <see cref="T:System.Web.IHttpHandler"/> instance.
